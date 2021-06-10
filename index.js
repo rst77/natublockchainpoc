f3035d151ae7e7dfb090f836d84c33ba9bac2889
pedrocarrijo <pedro.carrijo@oracle.com>
2021-06-10 08:34:38 -0300

    editado alguns pontos

--- index.js
+++ index.js
@@ -10,46 +10,4 @@ app.get('/',function(req,res) {
   
 });
 
-app.get('/sacas2',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexSacas2.html');
-  
-});
-
-app.get('/fabrica',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexFabrica.html');
-  
-});
-
-app.get('/distribuidora',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexDistribuidora.html');
-  
-});
-
-app.get('/maps',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexMapsCerto.html');
-  
-});
-
-app.get('/graph',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/graph.html');
-  
-});
-
-app.get('/smartcontract',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexSmart.html');
-  
-});
-
-app.get('/admin',function(req,res) {
-  res.type('text/html');
-  res.sendFile(__dirname+'/public/html/indexAdmin.html');
-  
-});
-
 app.listen(process.env.port || PORT);
